38706dba79313bba439838851fab35a449d5dc27
malanchew <mevolt@yandex.ru>
2021-09-26 21:38:23 +0300

    домашнее задание 8 Маланчев

---
+++ HW_8_1.py
@@ -0,0 +1,13 @@
+import re
+
+
+def email_parse(email_adress):
+    pattern = re.compile(r'^(?P<username>\w+)@(?P<domain>\w+\.\w+)$')
+    result = pattern.match(email_adress)
+    if not result:
+        raise ValueError(f'Адрес не валиден: {email_adress}')
+    return result.groupdict()
+
+
+print(email_parse('someone@geekbrains.ru'))
+print(email_parse('someone@geekbrainsru'))
